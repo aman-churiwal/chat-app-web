9dd59aac611f75bb08b5638beeef94a39025ffa4
Aman <amanchuriwal22@gmail.com>
2022-06-13 14:08:07 +0530

    added chat feature

--- js/client.js
+++ js/client.js
@@ -9,11 +9,23 @@ const append = (message, position) => {
     messageElement.innerText = message;
     messageElement.classList.add('message')
     messageElement.classList.add(position)
+    messageContainer.append(messageElement);
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.ariaValueMax;
+    append(`You: ${message}`)
+    socket.emit('send', message);
+    messageInput.value = '';
+})
 
 const name = prompt("Enter your name to join");
 socket.emit('new-user-joined', name);
 
-socket.on('user-joined', data => {
-
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right')
+})
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left')
 })
