c12d7517b7dee8a65080bea9c85bfdf47ea481e5
Aman <amanchuriwal22@gmail.com>
2022-06-14 10:40:30 +0530

    Added comments

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -9,14 +9,17 @@ const io = require('socket.io')(8000, {
 const users = {};
 
 io.on('connection', socket => {
+    // If any new user joins, let other users connected to the server know
     socket.on('new-user-joined', name => {
         users[socket.id] = name; socket.broadcast.emit('user-joined', name);
     })
 
+    // If someone sends a message, broadcast it to other users
     socket.on('send', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
     })
 
+    // If someone leaves the chat, let other users know
     socket.on('disconnect', message => {
         socket.broadcast.emit('left_chat', users[socket.id]);
         delete users[socket.id];
